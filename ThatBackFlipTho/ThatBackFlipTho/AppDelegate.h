4143bf831f8f47f754bc42d45a3a3d65a261ff03
maxveras <MaksimVeras@MaximsiMac.local>
2015-07-03 14:55:18 +0300

    Init Commit

---
+++ ThatBackFlipTho/ThatBackFlipTho/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThatBackFlipTho
+//
+//  Created by Максим on 03.07.15.
+//  Copyright (c) 2015 200GO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThatBackFlipTho/ThatBackFlipTho/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  ThatBackFlipTho
+//
+
+//  Copyright (c) 2015 200GO. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
